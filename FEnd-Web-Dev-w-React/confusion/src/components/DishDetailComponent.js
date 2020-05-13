96fc4c9ed90457e41510ddf1bb6bac0469fff125
Daniel Serrano <danashdserrano@hotmail.com>
2020-05-13 16:53:39 +0100

    Functional Components

--- FEnd-Web-Dev-w-React/confusion/src/components/DishDetailComponent.js
+++ FEnd-Web-Dev-w-React/confusion/src/components/DishDetailComponent.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle,
 } from 'reactstrap';
 
-class DishDetail extends Component {
-
-    constructor(props) {
-        super(props);
-
-    }
-
-    renderDish(dish) {
-        return (
+function RenderDish({ dish }) {
+    return (
+        <div className="col-12 col-md-5 m-1">
             <Card>
                 <CardImg top src={dish.image} alt={dish.name} />
                 <CardBody>
@@ -20,56 +14,55 @@ class DishDetail extends Component {
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
-        );
-    }
+        </div>
+    );
+}
 
-    renderComments(comments) {
-        const cmnts = comments.map(comment => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author}
+function RenderComments({ comments }) {
+    const cmnts = comments.map(comment => {
+        return (
+            <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, &nbsp;
                     {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit'
-                    }).format(new Date(comment.date))}
-                    </p>
-                </li>
-            )
-        })
+                    year: 'numeric',
+                    month: 'long',
+                    day: '2-digit'
+                }).format(new Date(comment.date))}
+                </p>
+            </li>
+        )
+    })
 
-        if (comments != null) {
-            return (
-                <div className='col-12 col-md-5 m-1'>
-                    <h4> Comments </h4>
-                    <ul className='list-unstyled'>
-                        {cmnts}
-                    </ul>
-    
-                </div>
-            )
-        } else {
-            return (<div></div>)
-        }
+    if (comments != null) {
+        return (
+            <div className='col-12 col-md-5 m-1'>
+                <h4> Comments </h4>
+                <ul className='list-unstyled'>
+                    {cmnts}
+                </ul>
+
+            </div>
+        )
+    } else {
+        return (<div></div>)
     }
+}
 
-    render() {
-        if (this.props.dish != null)
-            return (
+const DishDetail = (props) => {
+    if (props.dish != null)
+        return (
+            <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    {this.renderComments(this.props.dish.comments)}
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.dish.comments}/>
                 </div>
-
-            );
-        else
-            return (
-                <div></div>
-            );
-    }
+            </div >
+        );
+    else
+        return (
+            <div></div >
+        );
 }
 
 export default DishDetail;
